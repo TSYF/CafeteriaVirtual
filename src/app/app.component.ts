6ed89a1b8d74df5ab2451fb18c67fdcebb06712c
TSYF <tsyf1999@gmail.com>
2023-09-08 21:01:27 -0300

    CV-1 First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  public appPages = [
+    { title: 'Menú cafetería', url: '/folder/inbox', icon: 'restaurant' },
+    { title: 'Configuración', url: '/folder/outbox', icon: 'cog' },
+    { title: 'Favoritos', url: '/folder/favorites', icon: 'heart' },
+    { title: 'Pedidos', url: '/folder/archived', icon: 'archive' },
+    { title: 'Asociar tarjeta', url: '/folder/trash', icon: 'journal' },
+    { title: 'Ayuda', url: '/folder/spam', icon: 'help' },
+  ];
+  public labels = ['Productos destacados', 'Cafeterias', 'Notas'];
   constructor() {}
 }
